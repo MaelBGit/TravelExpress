c6af381ea3d0db43b9ff2606f2d882455e73d9da
Maxime <Maxime@PC-MAXIME>
2017-05-26 19:12:58 -0400

    Classes : Car Preference

---
+++ TravelExpress/TravelExpress/Preference.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace TravelExpress
+{
+    public class Preference
+    {
+        private int idPref;
+        private int idP;
+        private bool fumeur;
+        private string autre;
+
+        #region Get/Set
+        public int IdPref
+        {
+            get
+            {
+                return idPref;
+            }
+
+            set
+            {
+                idPref = value;
+            }
+        }
+
+        public int IdP
+        {
+            get
+            {
+                return idP;
+            }
+
+            set
+            {
+                idP = value;
+            }
+        }
+
+        public bool Fumeur
+        {
+            get
+            {
+                return fumeur;
+            }
+
+            set
+            {
+                fumeur = value;
+            }
+        }
+
+        public string Autre
+        {
+            get
+            {
+                return autre;
+            }
+
+            set
+            {
+                autre = value;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ TravelExpress/TravelExpress/Voiture.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace TravelExpress
+{
+    public class Car
+    {
+        private int idV;
+        private string marque;
+        private string modele;
+        private int annee;
+        private int places;
+        private string type;
+        private string plaque;
+        private int idp;
+
+        #region Get/Set
+        public int IdV
+        {
+            get
+            {
+                return idV;
+            }
+
+            set
+            {
+                idV = value;
+            }
+        }
+
+        public string Marque
+        {
+            get
+            {
+                return marque;
+            }
+
+            set
+            {
+                marque = value;
+            }
+        }
+
+        public string Modele
+        {
+            get
+            {
+                return modele;
+            }
+
+            set
+            {
+                modele = value;
+            }
+        }
+
+        public int Annee
+        {
+            get
+            {
+                return annee;
+            }
+
+            set
+            {
+                annee = value;
+            }
+        }
+
+        public int Places
+        {
+            get
+            {
+                return places;
+            }
+
+            set
+            {
+                places = value;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+
+            set
+            {
+                type = value;
+            }
+        }
+
+        public string Plaque
+        {
+            get
+            {
+                return plaque;
+            }
+
+            set
+            {
+                plaque = value;
+            }
+        }
+
+        public int Idp
+        {
+            get
+            {
+                return idp;
+            }
+
+            set
+            {
+                idp = value;
+            }
+        }
+
+        #endregion
+    }
+}
