89ede7fbdeb40965f6c9afbb9a887a9374ce31cb
Maxime <Maxime@PC-MAXIME>
2017-05-26 10:36:56 -0400

    Init

---
+++ TravelExpress/TravelExpress/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TravelExpress.MvcApplication" Language="C#" %>
---
+++ TravelExpress/TravelExpress/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TravelExpress.Startup))]
+namespace TravelExpress
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
