31d58c96a87b08f7849aad2f3149dba934062028
Maxime <Maxime@PC-MAXIME>
2017-05-26 10:54:37 -0400

    User class

---
+++ TravelExpress/TravelExpress/User.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace TravelExpress
+{
+    public class User
+    {
+        private int id;
+        private string nom;
+        private string prenom;
+        private int age;
+        private string adresse;
+        private string ville;
+        private string codepostal;
+        private int tel;
+        private string pseudo;
+        private string password;
+        private string email;
+
+
+        #region Get/Set
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return prenom;
+            }
+
+            set
+            {
+                prenom = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+
+        public string Adresse
+        {
+            get
+            {
+                return adresse;
+            }
+
+            set
+            {
+                adresse = value;
+            }
+        }
+
+        public string Ville
+        {
+            get
+            {
+                return ville;
+            }
+
+            set
+            {
+                ville = value;
+            }
+        }
+
+        public string Codepostal
+        {
+            get
+            {
+                return codepostal;
+            }
+
+            set
+            {
+                codepostal = value;
+            }
+        }
+
+        public int Tel
+        {
+            get
+            {
+                return tel;
+            }
+
+            set
+            {
+                tel = value;
+            }
+        }
+
+        public string Pseudo
+        {
+            get
+            {
+                return pseudo;
+            }
+
+            set
+            {
+                pseudo = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        #endregion
+
+
+    }
+}
